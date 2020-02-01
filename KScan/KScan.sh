6b2838a3db25423e749a3a6bf0020e924318f570
Brady <bradyakruse@gmail.com>
2020-01-31 18:18:56 -0600

    working KScan

--- KScan/KScan.sh
+++ KScan/KScan.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-read -p '.PCD Filename without extension: ' filename
+filename=$1
 
 mkdir "${filename}"
 
-python3 PCDConverter.py "${filename}"
+if [ "$2" == '-s' ]
+then
+    python3 PCDConverter.py "${filename}"
 
-cp "${filename}".pcd "${filename}/${filename}.pcd"
+    cp "${filename}".pcd "${filename}/${filename}.pcd"
 
-mv "${filename}"_conv.pcd "${filename}/${filename}"_conv.pcd
+    mv "${filename}"_conv.pcd "${filename}/${filename}"_conv.pcd
 
-./convert_pcd_ascii_binary "${filename}/${filename}"_conv.pcd "${filename}/${filename}"_binary.pcd 1
+    ./convert_pcd_ascii_binary "${filename}/${filename}"_conv.pcd "${filename}/${filename}"_binary.pcd 1
+    
+    ./pcd2ply "${filename}/${filename}"_binary.pcd "${filename}/${filename}".ply
 
-./pcd2ply "${filename}/${filename}"_binary.pcd "${filename}/${filename}".ply
+else
+    cp "${filename}".pcd "${filename}/${filename}.pcd"
+
+    ./convert_pcd_ascii_binary "${filename}/${filename}".pcd "${filename}/\
+${filename}"_binary.pcd 1
+
+    ./pcd2ply "${filename}/${filename}"_binary.pcd "${filename}/${filename}".pl\
+y
+fi
+
+printf "\n[DONE] All files found in "${filename}"\n"
--- KScan/PCDConverter.py
+++ KScan/PCDConverter.py
@@ -11,9 +11,12 @@ file_new.write("HEIGHT 1\n")
 file_new.write(lines[9])
 file_new.write("DATA ascii\n")
 
+count = 0
 for line in lines[11:]:
+    count += 1
     temp = line.split(" ")
     file_new.write(temp[0] +  " " + temp[1] + " " + temp[2] + "\n")
+    print("\rPercent Complete: ", "%.2f" % (count/len(lines[11:]) * 100), "%", end = "")
 
 
 
