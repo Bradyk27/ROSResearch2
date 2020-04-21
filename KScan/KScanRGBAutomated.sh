3978cd956addb768aeb6133e6e306adea34e93aa
Brady <bradyakruse@gmail.com>
2020-04-20 21:11:46 -0500

    autuomated transfer

---
+++ KScan/KScanRGBAutomated.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#Consider using pipes here
+
+filename=$1
+
+mkdir "${filename}"
+
+python3 PCDConverterRGB.py "${filename}"
+
+cp "${filename}".pcd "${filename}/${filename}.pcd"
+
+mv "${filename}"_conv_RGB.pcd "${filename}/${filename}"_conv_RGB.pcd
+
+./convert_pcd_ascii_binary "${filename}/${filename}"_conv_RGB.pcd "${filename}/${filename}"_binary_RGB.pcd 1
+    
+./pcd2ply "${filename}/${filename}"_binary_RGB.pcd "../PointcloudRendering/Assets/pointcloud.ply"
+
+printf "\n[DONE] All files found in "${filename}"\n"
