fdb7b5065aa8a3de442ceb6f1814717668dabce8
Brady <bradyakruse@gmail.com>
2020-05-13 20:45:58 -0500

    my head hurtsssssssss

--- PointcloudRendering/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/PointcloudSubscriberFast.cs
+++ PointcloudRendering/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/PointcloudSubscriberFast.cs
@@ -9,7 +9,7 @@
 
 //Adapted from https://answers.ros.org/question/339483/ros-sharp-unity3d-import-pointcloud2/ & https://github.com/siemens/ros-sharp/blob/master/Libraries/RosBridgeClient/PointCloud.cs
 //Need to subscribe to TWO streams at once. Might need to do two classes that both output to a global variable
-//Compressed image *may* not work with the pointcloud generator
+//Compressed image *may* not work with the pointcloud generator. We shall see. Can't seem to subscribe to straight IMAGE streams though, which is annoying.
 
 using UnityEngine;
 using System;
@@ -22,19 +22,12 @@ namespace RosSharp.RosBridgeClient
     public class ImageSubscriberDepth : UnitySubscriber<MessageTypes.Sensor.Image> //There's a better way to do this
     {
         public Image depthImage;
-
-        public MeshRenderer meshRenderer;
-        private Texture2D texture2D;
-        private byte[] imageData;
         private bool isMessageReceived;
 
-        protected override void Start()
+        public void StartTransfer()
         {
             Debug.Log("DepthStart\n");
-            
 			base.Start();
-            texture2D = new Texture2D(1, 1);
-            meshRenderer.material = new Material(Shader.Find("Standard"));
         }
         private void Update()
         {
@@ -43,138 +36,140 @@ namespace RosSharp.RosBridgeClient
         }
 
         protected override void ReceiveMessage(MessageTypes.Sensor.Image Image)
-        {   Debug.Log("ReceiveMessageDepth\n");
+        {   
+            Debug.Log("ReceiveMessageDepth\n");
             depthImage = Image;
-            
-            imageData = Image.data;
             isMessageReceived = true;
         }
 
         private void ProcessMessage()
-        {   Debug.Log("ProcessMessageDepth\n");
-
-            texture2D.LoadImage(imageData);
-            texture2D.Apply();
-            meshRenderer.material.SetTexture("_MainTex", texture2D);
+        {   
+            Debug.Log("ProcessMessageDepth\n");
             isMessageReceived = false;
         }
     }
 
-    // public class ImageSubscriberColor : UnitySubscriber<MessageTypes.Sensor.Image> //There's a better way to do this
-    // {
-    //     public Image colorImage;
-    //     private bool isMessageReceived;
+    public class ImageSubscriberColor : UnitySubscriber<MessageTypes.Sensor.Image> //There's a better way to do this
+    {
+        public Image colorImage;
+        private bool isMessageReceived;
 
-    //     protected override void Start()
-    //     {   
-    //         Debug.Log("ColorStart");
-	// 		base.Start();
-    //     }
-    //     private void Update()
-    //     {
-    //         if (isMessageReceived)
-    //             ProcessMessage();
-    //     }
+        public void StartTransfer()
+        {   
+            Debug.Log("ColorStart");
+			base.Start();
+        }
+        private void Update()
+        {
+            if (isMessageReceived)
+                ProcessMessage();
+        }
 
-    //     protected override void ReceiveMessage(MessageTypes.Sensor.Image Image)
-    //     {   
-    //         Debug.Log("ReceiveMessageColor\n");
-    //         colorImage = Image;
-    //         isMessageReceived = true;
-    //     }
+        protected override void ReceiveMessage(MessageTypes.Sensor.Image Image)
+        {   
+            Debug.Log("ReceiveMessageColor\n");
+            colorImage = Image;
+            isMessageReceived = true;
+        }
 
-    //     private void ProcessMessage()
-    //     {
-    //         Debug.Log("ProcessMessageColor\n");
-    //         isMessageReceived = false;
-    //     }
-    // }
+        private void ProcessMessage()
+        {
+            Debug.Log("ProcessMessageColor\n");
+            isMessageReceived = false;
+        }
+    }
 
 
-    // public class PointcloudSubscriberFast : MonoBehaviour
-    // { 
-    //     //Mesh variables
-    //     public RgbPoint3[] Points;
-    //     private Mesh mesh;
-    //     public MeshRenderer meshRenderer;
-    //     Vector3[] vertices;
-    //     Color[] colors;
-    //     public ImageSubscriberColor colorImage;
-    //     public ImageSubscriberDepth depthImage;
+    public class PointcloudSubscriberFast : MonoBehaviour
+    { 
+        //Mesh variables
+        public RgbPoint3[] Points;
+        private Mesh mesh;
+        public MeshRenderer meshRenderer;
+        Vector3[] vertices;
+        Color[] colors;
+        public ImageSubscriberColor colorImage;
+        public ImageSubscriberDepth depthImage;
 
-    //     void Start()
-    //     {
-    //         Debug.Log("Start\n");
-    //         mesh = new Mesh();
-    //         GetComponent<MeshFilter>().mesh = mesh;
-    //         meshRenderer.material = new Material(Shader.Find("Custom/VertexColor"));
-    //     }
+        void Start()
+        {
+            Debug.Log("Start\n");
+            mesh = new Mesh();
+            GetComponent<MeshFilter>().mesh = mesh;
+            meshRenderer.material = new Material(Shader.Find("Custom/VertexColor"));
+            colorImage = new ImageSubscriberColor();
+            colorImage.StartTransfer(); //Probably need to manually force classes to receive messages & update and stuff. Actually there's surely a better way to do this...
+            //READ CONSOLE WARNING ABOUT NEW COMMAND
+            //FIGURE OUT HOW TO PASS VARIABLES BETWEEN SCRIPTS            
+            depthImage = new ImageSubscriberDepth();
 
-    //     void Update() //Need to consider how when and how things are updated
-    //     {
-    //         GeneratePointcloud();
-    //     }
+        }
 
-    //     private void GeneratePointcloud()
-    //     {
-    //         uint width = depthImage.depthImage.width;
-    //         uint height = depthImage.depthImage.height;
-    //         float invFocal = 1.0f; //Remove focal variable
+        void Update() //Need to consider how when and how things are updated
+        {
+            GeneratePointcloud();
+        }
 
-    //         Points = new RgbPoint3[width * height];
+        private void GeneratePointcloud()
+        {
+            // uint width = depthImage.depthImage.width;
+            // uint height = depthImage.depthImage.height;
+            // float invFocal = 1.0f; //Remove focal variable
 
-    //         for (uint v = 0; v < height; v++)
-    //         {
-    //             for (uint u = 0; u < width; u++)
-    //             {
-    //                 float depth = 0;// depthImage[u, v]; //It appears this is just being filled with NaN types. Why?
-    //                 if (depth == 0)
-    //                 {
-    //                     Points[u + v * width].x = float.NaN;
-    //                     Points[u + v * width].y = float.NaN;
-    //                     Points[u + v * width].z = float.NaN;
-    //                     Points[u + v * width].rgb = new int[] { 0, 0, 0 };
-    //                 }
-    //                 else
-    //                 {
-    //                     Points[u + v * width].z = depth * invFocal;
-    //                     Points[u + v * width].x = u * depth * invFocal;
-    //                     Points[u + v * width].y = v * depth * invFocal;
-    //                     Points[u + v * width].rgb = new int[] { 0, 0, 0 };// rgbImage[u, v];
-    //                 }
-    //             }
-    //         }
+            // Points = new RgbPoint3[width * height];
 
-    //         vertices = new Vector3[Points.Length];
-    //         colors = new Color[Points.Length];
-    //         for (var i = 0; i < Points.Length; i++)
-    //         {
-    //             vertices[i].x = Points[i].x;
-    //             vertices[i].y = Points[i].z;
-    //             vertices[i].z = Points[i].y;
-    //             colors[i].r = (float)((float)Points[i].rgb[0] / 255.0);
-    //             colors[i].g = (float)((float)Points[i].rgb[1] / 255.0);
-    //             colors[i].b = (float)((float)Points[i].rgb[2] / 255.0);
-    //             colors[i].a = 1.0F; 
-    //             //Debug.Log("Colors: " + colors[i].ToString());
-    //             //Debug.Log("Vertex Colors: " + Points[i].rgb[0] + " " + Points[i].rgb[1] + " " + Points[i].rgb[2] + "\n");
-    //         }
+            // for (uint v = 0; v < height; v++)
+            // {
+            //     for (uint u = 0; u < width; u++)
+            //     {
+            //         float depth = 0;// depthImage[u, v]; //It appears this is just being filled with NaN types. Why?
+            //         if (depth == 0)
+            //         {
+            //             Points[u + v * width].x = float.NaN;
+            //             Points[u + v * width].y = float.NaN;
+            //             Points[u + v * width].z = float.NaN;
+            //             Points[u + v * width].rgb = new int[] { 0, 0, 0 };
+            //         }
+            //         else
+            //         {
+            //             Points[u + v * width].z = depth * invFocal;
+            //             Points[u + v * width].x = u * depth * invFocal;
+            //             Points[u + v * width].y = v * depth * invFocal;
+            //             Points[u + v * width].rgb = new int[] { 0, 0, 0 };// rgbImage[u, v];
+            //         }
+            //     }
+            // }
 
-    //         mesh.Clear(); //Removed try / catch loop
-    //         mesh.vertices = vertices;
-    //         mesh.colors = colors;
+            // vertices = new Vector3[Points.Length];
+            // colors = new Color[Points.Length];
+            // for (var i = 0; i < Points.Length; i++)
+            // {
+            //     vertices[i].x = Points[i].x;
+            //     vertices[i].y = Points[i].z;
+            //     vertices[i].z = Points[i].y;
+            //     colors[i].r = (float)((float)Points[i].rgb[0] / 255.0);
+            //     colors[i].g = (float)((float)Points[i].rgb[1] / 255.0);
+            //     colors[i].b = (float)((float)Points[i].rgb[2] / 255.0);
+            //     colors[i].a = 1.0F; 
+            //     //Debug.Log("Colors: " + colors[i].ToString());
+            //     //Debug.Log("Vertex Colors: " + Points[i].rgb[0] + " " + Points[i].rgb[1] + " " + Points[i].rgb[2] + "\n");
+            // }
 
-    //         //Graphs mesh as points. Works with /rtabmap/cloud_map & /voxel_cloud
-    //         int[] indices = new int[vertices.Length];
-    //         for(int i = 0; i < mesh.vertices.Length; i++){
-    //             indices[i] = i;
-    //         }
-    //         mesh.SetIndices(indices, MeshTopology.Points, 0);
-    //         mesh.RecalculateBounds();
-    //         //AssetDatabase.CreateAsset(mesh, "Assets/testMeshColorRTABMap4.asset");
-    //         //AssetDatabase.SaveAssets();
+            // mesh.Clear(); //Removed try / catch loop
+            // mesh.vertices = vertices;
+            // mesh.colors = colors;
 
-    //     }
+            // //Graphs mesh as points. Works with /rtabmap/cloud_map & /voxel_cloud
+            // int[] indices = new int[vertices.Length];
+            // for(int i = 0; i < mesh.vertices.Length; i++){
+            //     indices[i] = i;
+            // }
+            // mesh.SetIndices(indices, MeshTopology.Points, 0);
+            // mesh.RecalculateBounds();
+            // //AssetDatabase.CreateAsset(mesh, "Assets/testMeshColorRTABMap4.asset");
+            // //AssetDatabase.SaveAssets();
 
-    // }   
+        }
+
+    }   
 }
